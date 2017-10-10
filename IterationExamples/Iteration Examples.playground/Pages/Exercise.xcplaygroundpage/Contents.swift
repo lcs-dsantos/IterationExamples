3ce40ec251666fcdea63b6691bf42fe2eed30d41
Santos, Diego <dsantos@lcs-mba3217.lcs.on.ca>
2017-10-10 15:01:59 -0400

    second loop

--- IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -32,7 +32,7 @@ let canvas = Canvas(width: 300, height: 300)
 
 for i in stride(from: 0, through: 300, by: 20){
     canvas.drawLine(fromX: i, fromY: 0, toX: 300, toY: i)
-    canvas.drawLine(fromX: i, fromY: 0, toX: 300, toY: i)
+    canvas.drawLine(fromX: i, fromY: 300, toX: 0, toY: i)
 
 }
 
